fee756eabafed99ffdfe06f98128d6e7ef7a16a9
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-20 16:31:20 +0500

    commit

--- website/models.py
+++ website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Users(AbstractUser):
+    is_organiser = models.BooleanField(default=False)
+    is_agent = models.BooleanField(default=False)
--- yadro/settings.py
+++ yadro/settings.py
@@ -115,3 +115,6 @@ STATICFILES_DIRS =(
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'website.Users'
