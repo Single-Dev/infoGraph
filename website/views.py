0f3ceeab3d6aac0147ad1fa353481f9064324daa
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-08-26 08:26:29 +0500

    commit

--- website/views.py
+++ website/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views import generic
 
 class Home(generic.TemplateView):
-    template_name = "layouts/base.html"
+    template_name = "pages/home.html"
