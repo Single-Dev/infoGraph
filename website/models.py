492713d78b8e2ca3b6101c9ee9c569c21b5f17ce
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-03 16:02:22 +0500

    commit

--- website/models.py
+++ website/models.py
@@ -50,7 +50,6 @@ class Dashboard(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Dashboard,on_delete=models.CASCADE,related_name='comments')
     name = models.CharField(max_length=80)
-    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
