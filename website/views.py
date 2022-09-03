5df793b1be3403cfe34457fcaaed6111d34beec8
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-03 15:25:39 +0500

    commit

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
-from urllib import request
-from django.shortcuts import render
+from .form import *
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import *
 
@@ -12,3 +12,31 @@ def test(request, name):
         "dash_test":dash_name
     }
     return render(request, "test.html", context)
+
+
+
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Dashboard, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    context= {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form}
+    return render(request, template_name, context)
