935a41c175b4277496dc32a802fc4f4bdcdee0b8
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-03 16:14:22 +0500

    commit

--- website/admin.py
+++ website/admin.py
@@ -6,13 +6,4 @@ admin.site.register(Countrys)
 admin.site.register(Profile)
 admin.site.register(Dashboard)
 admin.site.register(Category)
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on', 'active')
-    list_filter = ('active', 'created_on')
-    search_fields = ('name', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(active=True)
+admin.site.register(Comment)
--- website/models.py
+++ website/models.py
@@ -55,4 +55,4 @@ class Comment(models.Model):
 
 
     def __str__(self):
-        return f'{self.body} by {self.name}'
+        return self.name
