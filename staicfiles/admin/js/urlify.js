300a97757921834619f058b04b47b88030fc10fb
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-06 22:00:09 +0500

    commit

--- staicfiles/admin/js/urlify.js
+++ staicfiles/admin/js/urlify.js
@@ -126,7 +126,7 @@
     ];
 
     const Downcoder = {
-        'Initialize': function() {
+        'Initialize': function () {
             if (Downcoder.map) { // already made
                 return;
             }
@@ -140,7 +140,7 @@
 
     function downcode(slug) {
         Downcoder.Initialize();
-        return slug.replace(Downcoder.regex, function(m) {
+        return slug.replace(Downcoder.regex, function (m) {
             return Downcoder.map[m];
         });
     }
