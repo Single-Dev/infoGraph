97eded6d31f0cbbad54a177e7a3aac6f726b6ac5
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-13 19:26:25 +0500

    get staticfiles for deploy

--- yadro/settings.py
+++ yadro/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS =(
     BASE_DIR / 'static',
 )
 
+STATIC_ROOT = BASE_DIR / 'staicfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
