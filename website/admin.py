8b620d27feb3839dba0adda731c14b09e1dae695
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-25 13:32:16 +0500

    delete files

--- website/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Users)
-admin.site.register(Countrys)
-admin.site.register(Profile)
-admin.site.register(Dashboard)
-admin.site.register(Category)
-admin.site.register(Comment)
--- website/form.py
+++
@@ -1,8 +0,0 @@
-from .models import Comment
-from django import forms
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('authour',  'message')
