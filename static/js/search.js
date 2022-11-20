5bf26d164c1093581f8d01a607526376e875a06b
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-11-20 12:56:02 +0500

    update search reslut page

---
+++ static/js/search.js
@@ -0,0 +1,4 @@
+let users_count_div = document.getElementById("users_count_div")
+let charts_count_div = document.getElementById("charts_count_div")
+let users_div = document.querySelector("users_div")
+let chart_div = document.querySelector('.chart_div')
