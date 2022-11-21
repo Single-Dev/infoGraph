650124506a0fef4f6d5c2564abc9e30f005f9d96
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-11-21 23:07:18 +0500

    simple changes

--- chart/models.py
+++ chart/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.utils import timezone
 from PIL import Image
 
