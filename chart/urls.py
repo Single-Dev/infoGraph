749f4576ee44970e1ada82b0fcf10d993d76fd3d
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-11-21 22:19:55 +0500

    added settings

--- chart/urls.py
+++ chart/urls.py
@@ -4,15 +4,16 @@ app_name = "app"
 
 urlpatterns = [
     path('', home, name="home"),
-    path("signup/", signup, name="signup"),
     path("<str:username>", ProfileView, name="profile"),
     path("f/<str:author>/",followToggle, name="follow"),
+    path("password_change/", password_change, name="password_change"),
+    path("settings/", settings, name="settings"),
     path("new/", NewChartView, name="new"),
+    path("signup/", signup, name="signup"),
     path("stats/<slug:slug>/", ChartView, name="chart"),
     path("edit/<str:slug>/", UpdateChartView, name="update_chart"),
     path("delete/<str:slug>/", deleteChartView, name="delete_chart"),
     path("edit/<str:slug>/<int:pk>", UpdateElementView, name="edit_element"),
     path("delete/<str:slug>/<int:pk>", deleteElementView, name="delete_element"),
     path("result/", results, name="results"),
-    path("password_change/", password_change, name="password_change")
 ]
--- chart/views.py
+++ chart/views.py
@@ -97,21 +97,38 @@ def ProfileView(request, username):
         }
         return render(request, 'pages/profile.html', context)
 
-@login_required(login_url='/login/')
 def followToggle(request, author):
-    authorObj = User.objects.get(username=author)
-    currentUserObj = User.objects.get(username=request.user.username)
-    following = authorObj.following.all()
+    if request.user.is_authenticated:
+        authorObj = User.objects.get(username=author)
+        currentUserObj = User.objects.get(username=request.user.username)
+        following = authorObj.following.all()
 
-    if author != currentUserObj.username:
-        if currentUserObj in following:
-            authorObj.following.remove(currentUserObj.id)
-        else:
-            authorObj.following.add(currentUserObj.id)
+        if author != currentUserObj.username:
+            if currentUserObj in following:
+                authorObj.following.remove(currentUserObj.id)
+            else:
+                authorObj.following.add(currentUserObj.id)
 
-    return HttpResponseRedirect(reverse("app:profile", args=[authorObj.username]))
+        return HttpResponseRedirect(reverse("app:profile", args=[authorObj.username]))
+    else:
+        return redirect('/signup/?next=%s' % request.path)
 
+def password_change(request):
+    form  = PasswordChangeForm(user=request.user, data=request.POST)
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect("app:profile", request.user.username)
+    context={
+        "form":form,
+    }
+    return render(request, "pages/settings/password_change.html", context )
 
+def settings(request):
+    
+    return render(request, "pages/settings/settings.html")
 
 @login_required(login_url='/login/')
 def NewChartView(request):
@@ -266,19 +283,3 @@ def results(request):
     }
     return render(request, "pages/result.html", context)
 
-
-def password_change(request):
-    if request.user.is_authenticated:
-        form  = PasswordChangeForm(user=request.user, data=request.POST)
-        if request.method == 'POST':
-            form = PasswordChangeForm(user=request.user, data=request.POST)
-            if form.is_valid():
-                form.save()
-                update_session_auth_hash(request, form.user)
-                return redirect("app:profile", request.user.username)
-        context={
-            "form":form,
-        }
-        return render(request, "registration/change_password.html", context)
-    else:
-        return redirect('/login/?next=%s' % request.path)
