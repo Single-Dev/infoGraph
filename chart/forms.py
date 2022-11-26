462b891d482b37239dc75cbdc4704f235d419e31
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-11-26 22:40:00 +0500

    update profile

--- chart/forms.py
+++ chart/forms.py
@@ -1,7 +1,8 @@
-from .models import *
-from django import forms
+from django.core.files.images import get_image_dimensions
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django import forms
+from .models import *
 
 User = get_user_model()
 
@@ -46,6 +47,39 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["image", "bio"]
+    
+    def clean_avatar(self):
+        avatar = self.cleaned_data['image']
+
+        try:
+            w, h = get_image_dimensions(avatar)
+
+            #validate dimensions
+            max_width = max_height = 100
+            if w == max_width or h == max_height:
+                raise forms.ValidationError(
+                    u'Please use an image that is '
+                     '%s x %s pixels or smaller.' % (max_width, max_height))
+
+            #validate content type
+            main, sub = avatar.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG, '
+                    'GIF or PNG image.')
+
+            #validate file size
+            if len(avatar) > (20 * 1024):
+                raise forms.ValidationError(
+                    u'Avatar file size may not exceed 20k.')
+
+        except AttributeError:
+            """
+            Handles case when we are updating the user profile
+            and do not supply a new avatar
+            """
+            pass
+
+        return avatar
 
 class ContactUsForm(forms.ModelForm):
     class Meta:
--- chart/models.py
+++ chart/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from django.db import models
-from PIL import Image
+# from PIL import Image
 
 class MyUser(AbstractUser):
     is_organiser = models.BooleanField(default=False)
@@ -20,15 +20,15 @@ class Profile(models.Model):
     def __str__(self):
         return f"id: {self.id}, {self.custom_user}"
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super(Profile, self).save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 class Chart(models.Model):
     PIE = "Pie"
