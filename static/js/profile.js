0a59fb6fa6c37239641bf71db6865b259ef5d338
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-12 22:53:13 +0500

    `#update` profile

--- static/js/profile.js
+++ static/js/profile.js
@@ -2,17 +2,21 @@ let edit_btn = document.getElementById("edit_btn")
 let edit_profile_div = document.querySelector(".edit_profile_div")
 let form = edit_profile_div.querySelector("form")
 let update_btn = document.getElementById("update_btn")
+let alert_msg = document.getElementById('msg')
 
-edit_profile_div.style.display = localStorage.form_display
 edit_btn.addEventListener('click', function () {
-    edit_profile_div.style.display = localStorage.form_display
     localStorage.setItem("form_display", 'block')
 })
 
 update_btn.addEventListener('click', function(){
-    edit_profile_div.style.display = localStorage.form_display
-    localStorage.setItem("form_display", 'none')
+    if (alert_msg){
+        localStorage.setItem("form_display", 'none')
+    }
 })
+
+setInterval(() => {
+    edit_profile_div.style.display = localStorage.form_display
+}, 1);
 
 // form.addEventListener("submit", e => {
 //     e.preventDefault()
