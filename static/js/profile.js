87b835233742bd9ea84849e1791f9492d2a8f2a8
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-12 22:36:37 +0500

    `update` profile.js

--- static/js/profile.js
+++ static/js/profile.js
@@ -3,8 +3,15 @@ let edit_profile_div = document.querySelector(".edit_profile_div")
 let form = edit_profile_div.querySelector("form")
 let update_btn = document.getElementById("update_btn")
 
+edit_profile_div.style.display = localStorage.form_display
 edit_btn.addEventListener('click', function () {
-    edit_profile_div.style.display = "block"
+    edit_profile_div.style.display = localStorage.form_display
+    localStorage.setItem("form_display", 'block')
+})
+
+update_btn.addEventListener('click', function(){
+    edit_profile_div.style.display = localStorage.form_display
+    localStorage.setItem("form_display", 'none')
 })
 
 // form.addEventListener("submit", e => {
