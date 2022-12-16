a59e3b94b972ec97bf42284ab0f6bb225eb90702
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-16 23:02:21 +0500

    update `templates/pages/helpers/success.html`

--- chart/urls.py
+++ chart/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="pages/settings/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='pages/settings/password_reset_complete.html'), name='password_reset_complete'),      
     path("settings/verify-request/", VerifyRequestView, name="verify_request_view"),
-    path("submit-success", SuccessView.as_view(), name="success_view"),
+    path("submit-success/", SuccessView.as_view(), name="success_view"),
 ]
